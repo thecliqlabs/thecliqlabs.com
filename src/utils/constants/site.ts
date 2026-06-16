38fff586aa984725f21202624d3bbec3a1df62ca
The Cliq Labs <thecliqlabs@gmail.com>
2026-06-16 11:29:47 -0400

    Remove src/utils/constants/site.ts

--- src/utils/constants/site.ts
+++
@@ -1,6 +0,0 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "CliqLabs";
-export const APP_DOMAIN = `https://${process.env.NEXT_PUBLIC_APP_DOMAIN || "thecliqlabs.com"}`;
-export const APP_HOSTNAMES = new Set([
-    process.env.NEXT_PUBLIC_APP_DOMAIN || "thecliqlabs.com",
-    `www.${process.env.NEXT_PUBLIC_APP_DOMAIN || "thecliqlabs.com"}`,
-]);
