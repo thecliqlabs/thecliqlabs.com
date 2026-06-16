8e468524689c90316e12e4fa8ba7ca0a71d79490
The Cliq Labs <thecliqlabs@gmail.com>
2026-06-16 11:28:40 -0400

    Remove src/app/not-found.tsx

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-export default function NotFound() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-            <Link href="/" className="mt-4 text-primary underline">Go Home</Link>
-        </div>
-    );
-}
