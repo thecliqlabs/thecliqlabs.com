d610349a74c38b00e9ebd58ec113c1a5e8208c09
The Cliq Labs <thecliqlabs@gmail.com>
2026-06-16 11:28:43 -0400

    Remove src/components/index.ts

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as AnimationContainer } from "./global/animation-container";
-export { default as MaxWidthWrapper } from "./global/max-width-wrapper";
-export { default as PricingCards } from "./pricing-cards";
-export { default as Icons } from "./global/icons";
-export { default as Navbar } from "./navigation/navbar";
-export { default as Footer } from "./navigation/footer";
-export { default as MobileNavbar } from "./navigation/mobile-navbar";
-export { default as Blogs } from "./blog/blogs";
